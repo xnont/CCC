620c845b5ad4d36be94ee11076eca5b1937649d7
buttfa <1662332017@qq.com>
2025-03-31 20:13:16 +0800

    feat: Add scripts for installing and uninstalling ccc on the Windows operating system.

--- Makefile
+++ Makefile
@@ -1,3 +1,14 @@
+# Obtain OS type
+ifeq ($(shell uname), Linux)
+    OS_TYPE = linux
+else ifeq ($(shell uname), Darwin)
+    OS_TYPE = macos
+else ifeq ($(OS), Windows_NT)
+    OS_TYPE = windows
+else
+    OS_TYPE = unknown
+endif
+
 debug:
 	make -f ./ccc/Makefile COMPILE_FLAGS="-Og -g -std=c++17 -W -Wall"
 	make -f ./cccmain/Makefile COMPILE_FLAGS="-Og -g -std=c++17 -W -Wall"
@@ -20,3 +31,13 @@ dev:
 	export CCC_COMPILER=g++
 	export CCC_LIBRARY_PATH=$(shell pwd)/build/lib/
 	export CCC_INCLUDE_PATH=$(shell pwd)/build/inc/
+
+install:
+	mkdir -p $(shell echo $$HOME)/.ccc
+	cp -r ./build/* $(shell echo $$HOME)/.ccc/
+ifeq ($(OS_TYPE), windows)
+	python ./script/windows_installer.py
+endif
+
+uninstall:
+	rm -rf $(shell echo $$HOME)/.ccc
---
+++ script/windows_installer.py
@@ -0,0 +1,35 @@
+import os
+import winreg
+
+
+def get_user_environment_variable(name: str) -> str:
+    try:
+        with winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER) as hkey:
+            with winreg.OpenKey(hkey, r'Environment', 0, winreg.KEY_ALL_ACCESS) as sub_key:
+                value, _ = winreg.QueryValueEx(sub_key, name)
+    except:
+        return ""
+    
+    return value
+
+def set_user_environment_variable(name: str, value: str):
+    with winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER) as hkey:
+        with winreg.OpenKey(hkey, r'Environment', 0, winreg.KEY_ALL_ACCESS) as sub_key:
+            winreg.SetValueEx(sub_key, name, 0, winreg.REG_EXPAND_SZ, value)
+
+if __name__ == '__main__':
+
+    # Set three environment variables required for ccc runtime.
+    if get_user_environment_variable('CCC_COMPILER') == '':
+        set_user_environment_variable('CCC_COMPILER', 'g++')
+    if get_user_environment_variable('CCC_INCLUDE_PATH') == '':
+        set_user_environment_variable('CCC_INCLUDE_PATH', r'%USERPROFILE%\.ccc\inc')
+    if get_user_environment_variable('CCC_LIBRARY_PATH') == '':
+        set_user_environment_variable('CCC_LIBRARY_PATH', r'%USERPROFILE%\.ccc\lib')
+    
+    # Add the directory where the ccc executable program is located to the Path variable.
+    Path_Value = get_user_environment_variable('Path')
+    if r"%USERPROFILE%\.ccc\bin" not in Path_Value.split(';'):
+        set_user_environment_variable('Path', Path_Value + r";%USERPROFILE%\.ccc\bin")
+
+    pass
